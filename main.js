211379383f1d539ab3b08584cbb7958ce9bc8bed
NaoMeLigo <94879340+NaoMeLigo@users.noreply.github.com>
2022-06-15 19:03:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//==== ALL ============================
 window.addEventListener('scroll', onScroll)
 
 onScroll()
@@ -10,8 +10,6 @@ function onScroll(){
 }
 
 // BACK TO TOP
-
-
 function showBackToTop(){
     if(scrollY > 250){
         backToTop.classList.add('show')
@@ -19,3 +17,27 @@ function showBackToTop(){
         backToTop.classList.remove('show')
     }
 }
+
+
+
+//==== DADOS.html ============================
+
+
+class Dado{
+    constructor(mod, dados){
+        this.mod= mod
+        this.dados= dados
+    }
+    dices(){
+        let dSomas= 0
+
+        for(let i=-1; i >= this.dados; i++){
+            return dSomas += this.mod
+        }
+
+        return dSomas
+    }
+}
+let d20 = new Dado(3, 1)
+
+console.log(d20.dices())
