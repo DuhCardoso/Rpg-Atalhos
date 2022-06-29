09c195652c046f13560026ea97591222bb31ef3e
Eduardo Cardoso de Jesus <eduardo.cardoso.j221@gmail.com>
2022-06-29 18:44:28 -0300

    init pc-git

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const d4 = () => {
 
     // Critico
     if(dSomas -mod.value == 4 || dSomas -mod.value == 1){
-        return result.innerHTML = "<h2 style='color: red;'>" + dSomas + "</h2>"
+        return result.innerHTML = '<img src="./media/d20.jpg" alt="imagem de um dado de 20 lados">'+"<h2 style='color: red;'>" + dSomas + "</h2>"
     }
 
     return result.innerHTML = "<h2>" + dSomas + "</h2>"
@@ -134,26 +134,39 @@ const d12 = () => {
     return result.innerHTML = "<h2>" + dSomas + "</h2>"
 }
 const d20 = () => {
-    let dSomas= 0
     let nD= nDados.value
-    
+
+
     // Erro alert
     if(nD <= 0){
-        return result.innerHTML = "<h3>" + "Quantidade de dados invelida" + "</h3>"
-        
+        return result.innerHTML = "<h2>" + "Quantidade de dados invelida" + "</h2>"
     }
+
     
     // Rolagem do dado
-    for(let i=0; i < nD; i++){
-        dSomas =+ mod.value +(Math.floor(Math.random() *20 +1))
-    }
+    let i = 0
+    while(i < nD){
+        let dSomas=+ mod.value +(Math.floor(Math.random() *20 +1))
+        let resultado
 
-    // Critico
-    if(dSomas -mod.value == 20 || dSomas -mod.value == 1){
-        return result.innerHTML = "<h2 style='color: red;'>" + dSomas + "</h2>"
-    }
+        // Result
+        if(i == 0){
+            resultado = result.innerHTML = "<div>" + '<img src="./media/d20.jpg" alt="imagem de um dado de 20 lados">'+"<h2>" + dSomas + "</h2>" + "</div>"
+        }else if(i>=1){ // Mais de 1 dado
+            resultado = result.innerHTML = +"<div>" + '<img src="./media/d20.jpg" alt="imagem de um dado de 20 lados">'+"<h2>" + dSomas + "</h2>" + "</div>"
+        }
 
-    return result.innerHTML = "<h2>" + dSomas + "</h2>"
+        // Result Crit
+        if((dSomas -mod.value == 20 || dSomas -mod.value == 1) && i==0){
+            resultado = result.innerHTML = "<div>" + '<img src="./media/d20.jpg" alt="imagem de um dado de 20 lados">'+"<h2 style='color: red;'>" + dSomas + "</h2>" + "</div>"
+        
+        }else if((dSomas -mod.value == 20 || dSomas -mod.value == 1) && i>=1){
+            resultado = result.innerHTML = +"<div>" + '<img src="./media/d20.jpg" alt="imagem de um dado de 20 lados">'+"<h2 style='color: red;'>" + dSomas + "</h2>" + "</div>"
+        }
+
+        i++
+        return resultado
+    }
 }
 const d100 = () => {
     let dSomas= 0
@@ -177,3 +190,12 @@ const d100 = () => {
     return result.innerHTML = "<h2>" + dSomas + "</h2>"
 }
 
+
+function test(){
+    for(let i = 0; i >= 10; i++) {
+        // codigo
+        console.log(i) // console: 1;  2;  3;  4;  5;  6;  7;  8;  9;  10
+    }
+}
+
+console.log()
